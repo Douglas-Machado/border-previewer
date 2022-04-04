9d13ae040f569cef220b30c1a554342265f1c597
Douglas C Machado <douglasclaudino10.dc@gmail.com>
2022-04-04 15:59:50 -0300

    readme and formating the script

---
+++ README.md
@@ -0,0 +1,7 @@
+# Border Previewer
+
+## Begginner JS challenge 
+
+### Using
+
+* Clone the project and run the HTML file
--- assets/js/main.js
+++ assets/js/main.js
@@ -4,18 +4,27 @@ document.addEventListener('mouseup', e => {
   const el = e.target
   if(el.className === "top-left") {
     border.style.borderTopLeftRadius = `${el.value}%`;
-    document.querySelector('.top-left-display').innerHTML = `border-top-left-radius: ${border.style.borderTopLeftRadius};`;
+    document.querySelector('.top-left-display').innerHTML = `
+    border-top-left-radius: ${border.style.borderTopLeftRadius};
+    `;
   }
+  
   if(el.className === "top-right") {
     border.style.borderTopRightRadius = `${el.value}%`;
-    document.querySelector('.top-right-display').innerHTML = `border-top-right-radius: ${border.style.borderTopRightRadius};`;
+    document.querySelector('.top-right-display').innerHTML = `
+    border-top-right-radius: ${border.style.borderTopRightRadius};
+    `;
   } 
   if(el.className === "bottom-right") {
     border.style.borderBottomRightRadius = `${el.value}%`;
-    document.querySelector('.bottom-right-display').innerHTML = `border-bottom-right-radius: ${border.style.borderBottomRightRadius};`;
+    document.querySelector('.bottom-right-display').innerHTML = `
+    border-bottom-right-radius: ${border.style.borderBottomRightRadius};
+    `;
   } 
   if(el.className === "bottom-left") {
     border.style.borderBottomLeftRadius = `${el.value}%`;
-    document.querySelector('.bottom-left-display').innerHTML = `border-bottom-left-radius: ${border.style.borderBottomLeftRadius};`;
+    document.querySelector('.bottom-left-display').innerHTML = `
+    border-bottom-left-radius: ${border.style.borderBottomLeftRadius};
+    `;
   }
 })
